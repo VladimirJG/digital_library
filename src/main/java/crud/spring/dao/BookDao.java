46e1581fbf6d57983aa0c28aba7c5f5865aeacc2
VladimirJG <vladimi85@gmail.com>
2023-11-03 11:00:38 +0300

    added dao book & reader

--- src/main/java/crud/spring/dao/BookDao.java
+++ src/main/java/crud/spring/dao/BookDao.java
@@ -1,5 +1,42 @@
 package crud.spring.dao;
 
-public class BookDao {
+import crud.spring.models.Book;
+import crud.spring.models.PersonReader;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class BookDao {
+private JdbcTemplate jdbcTemplate;
+
+    public BookDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Book> showAllBooks() {
+        return jdbcTemplate.query("SELECT * FROM book", new BeanPropertyRowMapper<>(Book.class));
+    }
+
+    public Optional<Book> showBook(int id) {
+        return jdbcTemplate.query("SELECT * FROM Book WHERE book_id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Book.class))
+                .stream().findAny();
+    }
+
+    public void saveBook(Book book) {
+        jdbcTemplate.update("INSERT INTO Book(book_name, author, year_of_production) VALUES (?,?,?)",
+                book.getName(),book.getAuthor(),book.getYearOfIssue());
+    }
+
+    public void updateBook(Book upBook, int id) {
+        jdbcTemplate.update("UPDATE book SET book_name=?, author=?,year_of_production=? WHERE book_id=?",
+                upBook.getName(),upBook.getAuthor(),upBook.getYearOfIssue(), id);
+    }
+
+    public void deleteBook(int id) {
+        jdbcTemplate.update("DELETE FROM book WHERE book_id=?", id);
+    }
 }
--- src/main/java/crud/spring/dao/PersonReaderDao.java
+++ src/main/java/crud/spring/dao/PersonReaderDao.java
@@ -1,10 +1,13 @@
 package crud.spring.dao;
 
 import crud.spring.models.PersonReader;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+import java.util.Optional;
+@Component
 public class PersonReaderDao {
     private JdbcTemplate jdbcTemplate;
 
@@ -12,8 +15,27 @@ public class PersonReaderDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<PersonReader> showAll(){
-//        jdbcTemplate.query()
-return null;
+    public List<PersonReader> showAllReaders() {
+        return jdbcTemplate.query("SELECT * FROM Person_Reader", new BeanPropertyRowMapper<>(PersonReader.class));
     }
+
+    public Optional<PersonReader> showReader(int id) {
+        return jdbcTemplate.query("SELECT * FROM Person_Reader WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(PersonReader.class))
+                .stream().findAny();
+    }
+
+    public void saveReader(PersonReader reader) {
+        jdbcTemplate.update("INSERT INTO Person_Reader(name,year_of_birthday) VALUES (?,?)",
+                reader.getName(), reader.getYearOfBirth());
+    }
+
+    public void updateReader(PersonReader upReader, int id) {
+        jdbcTemplate.update("UPDATE Person_Reader SET name=?, year_of_birthday=? WHERE id=?", upReader.getName(),
+                upReader.getYearOfBirth(), id);
+    }
+
+    public void deleteReader(int id) {
+        jdbcTemplate.update("DELETE FROM Person_reader WHERE id=?", id);
+    }
+
 }
